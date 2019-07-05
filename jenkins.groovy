8b962d87b6df260925e61f1a5e5f49c7432d67c5
joe61081 <joe61081@mastek.com>
2019-07-05 14:03:53 +0100

    changes

--- jenkins.groovy
+++ jenkins.groovy
@@ -5,47 +5,66 @@ pipeline{
 	triggers {
         githubPush()
     }
+	scm {
+		git(url: 'git@github.com:joe61081/helloworld-jenkins-pipeline.git',credentialsId:'joe-github-ssh-token')
+	}
 	agent any
 	stages{
 		stage('master'){
 			steps{
-				scm {
-					git(url: 'git@github.com:joe61081/helloworld-jenkins-pipeline.git',credentialsId:'joe-github-ssh-token')
-    			}
-				runmvn();
+				sh 'mvn install'
+				sh 'mvn run'
 			}
 
 		}
 		stage('feature'){
+			when{
+				branch "feature*"
+			}
 			steps{
 				runmvn();
 			}
 
 		}
 		stage('DEV'){
+			when{
+				tag "dev-*"
+			}
 			steps{
 				runmvn();
 			}
 		}
 		stage('SIT'){
+			when{
+				tag "sit-*"
+			}
 			steps{
 				runmvn();
 			}
 
 		}
 		stage('UAT'){
+			when{
+				tag "uat-*"
+			}
 			steps{
 				runmvn();
 			}
 
 		}
 		stage('PRE'){
+			when{
+				tag "pre-*"
+			}
 			steps{
 				runmvn();
 			}
 
 		}
 		stage('PRD'){
+			when{
+				tag "prd-*"
+			}
 			steps{
 				runmvn();
 			}
