5a923f9aae345214e999c26d8ccb2c7faa7a695f
joe61081 <joe61081@mastek.com>
2019-07-05 15:37:25 +0100

    withMaven

--- jenkins.groovy
+++ jenkins.groovy
@@ -12,29 +12,53 @@ pipeline{
 				scm {
 					git(url: 'git@github.com:joe61081/helloworld-jenkins-pipeline.git',credentialsId:'joe-github-ssh-token')
     			}
-				sh 'mvn --version'
+				withMaven(
+						// Maven installation declared in the Jenkins "Global Tool Configuration"
+						maven: 'maven-3.6.1',
+						// Maven settings.xml file defined with the Jenkins Config File Provider Plugin
+						// We recommend to define Maven settings.xml globally at the folder level using
+						// navigating to the folder configuration in the section "Pipeline Maven Configuration / Override global Maven configuration"
+						// or globally to the entire master navigating to  "Manage Jenkins / Global Tools Configuration"
+						mavenSettingsConfig: 'pom.xml') {
+
+					// Run the maven build
+					sh "mvn clean verify"
+
+				}
 				runmvn();
 			}
 
 		}
 		stage('feature'){
+			when{
+				branch "feature*"
+			}
 			steps{
 				runmvn();
 			}
 
 		}
 		stage('DEV'){
+			when{
+				tag "dev-*"
+			}
 			steps{
 				runmvn();
 			}
 		}
 		stage('SIT'){
+			when{
+				tag "sit-*"
+			}
 			steps{
 				runmvn();
 			}
 
 		}
 		stage('UAT'){
+			when{
+				tag "uat-*"
+			}
 			steps{
 				runmvn();
 			}
@@ -50,6 +74,9 @@ pipeline{
 
 		}
 		stage('PRD'){
+			when{
+				tag "prd-*"
+			}
 			steps{
 				runmvn();
 			}
