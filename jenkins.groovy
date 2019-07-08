5bd4fddee676661730ac8be96cfcfe67ff86fcb8
joe61081 <joe61081@mastek.com>
2019-07-08 11:19:34 +0100

    changes

--- jenkins.groovy
+++ jenkins.groovy
@@ -4,13 +4,13 @@ pipeline {
 	}
 	options { buildDiscarder(logRotator(numToKeepStr: '2', artifactNumToKeepStr: '2')) }
 
-
 	stages {
 		stage('Deploy') {
 			steps {
-
 				script {
 					sh "echo 'Hello World'"
+					sh 'mvn install'
+					sh 'mvn build'
 
 				}
 
