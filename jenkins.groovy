af18069176cfbb26b08338fd84c40cc715e12fbd
joe61081 <joe61081@mastek.com>
2019-07-09 14:19:58 +0100

    readd setup

--- jenkins.groovy
+++ jenkins.groovy
@@ -8,6 +8,11 @@ pipeline {
 	}
 
 	stages {
+		stage('Setup'){
+			steps{
+				checkout([$class: 'GitSCM', branches: [[name: '*/master'], [name: '*/develop'], [name: 'feature/*'], [name: 'release/*']], doGenerateSubmoduleConfigurations: false, extensions: [], submoduleCfg: [], userRemoteConfigs: [[credentialsId: 'joe-github-ssh-token', url: 'git@github.com:joe61081/helloworld-jenkins-pipeline.git']]])
+			}
+		}
 		stage('Deploy') {
 			when{
 				expression {env.GIT_BRANCH == 'origin/master'}
