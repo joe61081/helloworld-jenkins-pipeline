1a964b567911bf6329a434fd677c0132a7689012
joe61081 <joe61081@mastek.com>
2019-07-10 10:00:09 +0100

    add test

---
+++ src/test/java/com/example/helloworld/api/HelloWorldApiTests.java
@@ -0,0 +1,17 @@
+package com.example.helloworld.api;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+public class HelloWorldApiTests {
+
+    @Test
+    public void sayHelloTest(){
+        AtomicLong counter = new AtomicLong();
+        Saying saying = new Saying(counter.incrementAndGet(), "test");
+        assertEquals(1, saying.getId());
+        assertEquals("test", saying.getContent());
+    }
+}
